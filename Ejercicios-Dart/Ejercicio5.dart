6d3b9dbdb853925bf597842e5b4d97d02bf01c2a
juana <valentinawalles57@gmail.com>
2023-06-27 17:19:05 -0500

    Ejercicio de determinar el cuadrado

---
+++ Ejercicios-Dart/Ejercicio5.dart
@@ -0,0 +1,20 @@
+void main() {
+  /*Realizar en el lenguaje dart un programa que lea 3 numeros 
+  y determinar el cuadrado de cada uno de ellos */
+
+  // Leer los tres números
+ 
+  int numero1 = 3;
+  int numero2 = 4;
+  int numero3 = 5;
+
+  // Calcular el cuadrado de cada número
+  int cuadrado1 = numero1 * numero1;
+  int cuadrado2 = numero2 * numero2;
+  int cuadrado3 = numero3 * numero3;
+
+  // Mostrar los resultados
+  print('El cuadrado del primer número es: $cuadrado1');
+  print('El cuadrado del segundo número es: $cuadrado2');
+  print('El cuadrado del tercer número es: $cuadrado3');
+}
